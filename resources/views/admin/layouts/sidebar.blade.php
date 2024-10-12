2a3a5f8ad4baca7881480bfd465ed938ac469845
Lawrence Emeka <mrabarah@gmail.com>
2024-10-12 22:05:01 +0100

    add ColorController,work on color views and route

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -9,22 +9,36 @@
         <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
             <ul class="nav flex-column">
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" aria-current="page" href="#">
+                    <a class="nav-link d-flex align-items-center gap-2" aria-current="page" href="{{ Route('admin.index') }}">
                         <i class="fas fa-dashboard"></i>
                         Dashboard
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-2" aria-current="page" href="{{ Route('admin.colors.index') }}">
+                        <i class="fas fa-palette"></i>
+                        Colors
+                    </a>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-2" aria-current="page" href="{{ Route('admin.colors.index') }}">
+                        <i class="fas fa-dashboard"></i>
+                        Colors
+                    </a>
+                </li> -->
             </ul>
             <hr class="my-3">
             <ul class="nav flex-column mb-auto">
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" onclick="document.getElementById('#AdminLogoutForm).submit()" href="">
+                    <a class="nav-link d-flex align-items-center gap-2" onclick="document.getElementById('AdminLogoutForm').submit()" href="#">
                         <svg class="bi">
                             <use xlink:href="#door-closed" />
                         </svg>
                         Sign out
                     </a>
-                    <form id="AdminLogoutForm" action="{{ Route('admin.logout') }}" method="post"></form>
+                    <form id="AdminLogoutForm" action="{{ Route('admin.logout') }}" method="post">
+                        @csrf
+                    </form>
                 </li>
             </ul>
         </div>
